a76ea3750cb781060437b16fbb2ce4e53412211c
vijayinyoutube <vijaythevirtue@gmail.com>
2023-05-08 07:31:38 +0530

    initial commit

---
+++ lib/Presentation/Screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      
+    );
+  }
+}
