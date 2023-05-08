4c99c3244591a80466e2ad2ebe0f0db313bf6447
vijayinyoutube <vijaythevirtue@gmail.com>
2023-05-08 10:51:18 +0530

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'Presentation/Routes/generated_routes.dart';
-import 'Presentation/Screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
